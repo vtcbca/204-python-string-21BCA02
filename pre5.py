2e51222120f49e960ea2927e49e6eb60a67ee890
21BCA02 <96609308+21BCA02@users.noreply.github.com>
2022-03-24 11:20:46 +0530

    pro18
    this program have error....

---
+++ pre5.py
@@ -0,0 +1,27 @@
+#write a script to enter 5 string in a list and check and print string whose lenth has even number of charactor
+#do this udf..
+l=[]
+count=0
+def creatlist(l):
+    for i in range(5):
+      s=input("Enter a string:")
+      l.append(s)
+creatlist(l)
+
+def evenumber(l):
+    for i in l:
+        count=0
+        for j in i:
+            count+=1
+        if(l%2==0):
+            print(l)
+    return(count)
+evennumber(l)
+
+        
+
+        
+
+    
+
+
